c55399127ce40e55fcafd8839ee7a33b9d8c74a4
amenpunk <ondasycircuitos@gmail.com>
2021-09-18 16:25:39 -0600

    IOTA full stack :godmode:

--- signcore/Firma.py
+++ signcore/Firma.py
@@ -1,3 +1,4 @@
+from iota.transaction.validator import SUPPORTED_SPONGE
 from rest_framework.views import APIView
 from django.http import HttpResponse, JsonResponse,QueryDict, FileResponse
 from rest_framework import viewsets,status
@@ -5,6 +6,12 @@ from rest_framework.decorators import api_view
 from django.core.files.storage import default_storage
 from firebase_admin import firestore
 from datetime import datetime, timedelta
+from iota import Iota, TryteString, Address, Tag, ProposedTransaction
+from pprint import pprint
+import json
+
+
+
 import json
 
 class Firma(APIView):
@@ -29,12 +36,12 @@ class Firma(APIView):
     def post(self, request):
 
         try:
+
             data=json.loads(request.body)
-            print('rece -> ', data)
+
             uid = data['uid']
             qr = data['hash']
             db = firestore.client()
-
 
             ### find if already exist
             doc_ref = db.collection('firmas').document(qr).collection('users').document(uid)
@@ -51,9 +58,19 @@ class Firma(APIView):
             document = {
                 'uid' : uid,
                 'document' : qr,
-                'timestamp' :datetime.now(),
+                'timestamp' :datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)"),
             }
 
+            to_put = json.dumps(document)
+
+            api = Iota(adapter='https://nodes.devnet.iota.org:443', testnet=True)
+            my_data = TryteString.from_unicode(to_put)
+            my_address = Address.random(length=81)
+            my_tag = Tag(b'SCARLETSIGN')
+            tx = ProposedTransaction( address=my_address, value=0, tag=my_tag, message=my_data)
+            response = api.send_transfer([tx])
+            signature = response['bundle'][0].hash
+            document['signature'] = str(signature)
             save = ref.set(document)
             print(save)
 
