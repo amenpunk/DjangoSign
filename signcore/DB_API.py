5def170fdc34af78abdacd67bafcaca450b23fea
amenpunk <ondasycircuitos@gmail.com>
2021-10-16 18:02:29 -0600

    test consum file into django api

--- signcore/DB_API.py
+++ signcore/DB_API.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse,QueryDict, FileResponse
 from rest_framework import viewsets,status
 from rest_framework.decorators import api_view
 from django.core.files.storage import default_storage
+
 import ipfsApi
 
 import os
@@ -14,12 +15,17 @@ class API(APIView):
             data = QueryDict.dict(request.GET)
             print(data)
             file_name = data['id']
-            print(file_name)
-            pdf = os.getcwd() + default_storage.url(f'{file_name}.pdf')
-            response = FileResponse(open(pdf, 'rb'))
-            return response
+
+            ipfs = ipfsApi.Client('127.0.0.1', 8080)
+            content =  ipfs.cat(file_name)
+            file_response = FileResponse(content)
+            print('response -> ', file_response)
+            # print(file_name)
+            # pdf = os.getcwd() + default_storage.url(f'{file_name}.pdf')
+            # response = FileResponse(open(content, 'rb'))
+            # return JsonResponse({ 'status' : True })
         except BaseException as e:
-            print(e)
+            print('ERROR -> ',e)
             return JsonResponse({ 'status' : False })
 
     def post():
