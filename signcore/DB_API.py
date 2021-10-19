f01b952e6bd6580c7391dbf5b53507605381c1f0
amenpunk <ondasycircuitos@gmail.com>
2021-10-18 22:21:55 -0600

    [ADD] just another collection :godmode:

--- signcore/DB_API.py
+++ signcore/DB_API.py
@@ -1,26 +1,47 @@
 from rest_framework.views import APIView
 from django.http import HttpResponse, JsonResponse,QueryDict, FileResponse
-from rest_framework import viewsets,status
 from rest_framework.decorators import api_view
 from django.core.files.storage import default_storage
 import ipfsApi
 
-import os
+from firebase_admin import firestore
+import json
 
 class API(APIView):
 
     def get(self, request):
         try:
+
             data = QueryDict.dict(request.GET)
-            print(data)
-            file_name = data['id']
-            print(file_name)
-            pdf = os.getcwd() + default_storage.url(f'{file_name}.pdf')
-            response = FileResponse(open(pdf, 'rb'))
-            return response
+            hash = data['hash']
+            db = firestore.client()
+
+            ref = db.collection('firmas').document(hash).collection('users')
+            docs = ref.stream()
+            files = []
+
+            for doc in docs:
+                dic = doc.to_dict()
+                files.append(dic)
+
+            return JsonResponse( { 'status': True, "why" : 'success', 'data' : files })
+
+
         except BaseException as e:
             print(e)
             return JsonResponse({ 'status' : False })
+    # def get(self, request):
+    #     try:
+    #         data = QueryDict.dict(request.GET)
+    #         print(data)
+    #         file_name = data['id']
+    #         print(file_name)
+    #         pdf = os.getcwd() + default_storage.url(f'{file_name}.pdf')
+    #         response = FileResponse(open(pdf, 'rb'))
+    #         return response
+    #     except BaseException as e:
+    #         print(e)
+    #         return JsonResponse({ 'status' : False })
 
     def post():
         print('ok')
